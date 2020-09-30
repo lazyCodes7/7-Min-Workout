474fae646f276210bb2a91a5719e1dded09cd0e8
DazedTiara6667 <you@example.com>
2020-09-30 23:25:26 +0530

    added files related to timers and also added images for workouts

--- 7 MIn Workout/app/src/main/java/com/sevenminuteworkout/ExerciseActivity.kt
+++ 7 MIn Workout/app/src/main/java/com/sevenminuteworkout/ExerciseActivity.kt
@@ -1,11 +1,18 @@
 package com.sevenminuteworkout
 
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_exercise.*
 
 //TODO(Step 1 - Add an ExerciseActivity.)-->
 class ExerciseActivity : AppCompatActivity() {
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
+    private var restExerciseTimer : CountDownTimer? = null
+    private var restExerciseProgress = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +26,72 @@ class ExerciseActivity : AppCompatActivity() {
         toolbar_exercise_activity.setNavigationOnClickListener {
             onBackPressed()
         }
+        llExerciseView.visibility = View.GONE
+        setupRestView()
+
         //END
+    }
+
+    override fun onDestroy() {
+        if(restTimer!=null){
+            restProgress=0
+            restTimer!!.cancel()
+            super.onDestroy()
+
+        }
+        if(restExerciseTimer!=null){
+            restExerciseProgress=0
+            restExerciseTimer!!.cancel()
+        }
+    }
+
+
+    private fun restProgressBar(){
+        progressBar.progress = restProgress
+        restTimer = object : CountDownTimer(10000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+                restProgress++
+                progressBar.progress = 10-restProgress
+                tvTimer.text = (10-restProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,"You will know start the exercise",Toast.LENGTH_SHORT).show()
+                llRestView.visibility = View.GONE
+                llExerciseView.visibility=View.VISIBLE
+                setupExerciseRestView()
+
+            }
+        }.start()
+
+    }
+    private fun setupRestView(){
+        if(restTimer!=null){
+            restTimer!!.cancel()
+            restProgress=0
+        }
+        restProgressBar()
+    }
+    private fun setupExerciseRestView(){
+        if(restExerciseTimer!=null){
+            restExerciseTimer!!.cancel()
+            restExerciseProgress=0
+        }
+        restExerciseProgressBar()
+    }
+    private fun restExerciseProgressBar(){
+        progressExerciseBar.progress = restExerciseProgress
+        restExerciseTimer = object : CountDownTimer(30000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+                restExerciseProgress++
+                progressExerciseBar.progress = 30-restExerciseProgress
+                tvExerciseTimer.text = (30-restExerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,"You have finished this exercise",Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+
     }
 }
---
+++ 7 MIn Workout/app/src/main/java/com/sevenminuteworkout/ExerciseModel.kt
@@ -0,0 +1,29 @@
+package com.sevenminuteworkout
+
+class ExerciseModel(private var id: Int, private var name : String,private var image : Int, private var isCompleted: Boolean,private var isSelected: Boolean) {
+    fun getId():Int{
+        return id
+    }
+    fun setId(id : Int){
+        this.id = id
+    }
+    fun getName():String{
+        return name
+    }
+    fun setName(name: String){
+        this.name = name
+    }
+    fun getImage():Int{
+        return image
+    }
+    fun setImage(image : Int){
+        this.image = image
+    }
+    fun getisCompleted():Boolean{
+        return isCompleted
+    }
+    fun getisSelected():Boolean{
+        return isSelected
+    }
+
+}
